ceb5ea6fad925a72abbd4f4ef8301977bcd36272
victornext <victorbrs517@gmail.com>
2025-07-09 21:34:12 -0300

    Configuração do application.properties

---
+++ src/main/java/com/victornextgroup/encurtador/VictornextApplication.java
@@ -0,0 +1,13 @@
+package com.victornextgroup.encurtador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VictornextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VictornextApplication.class, args);
+	}
+
+}
