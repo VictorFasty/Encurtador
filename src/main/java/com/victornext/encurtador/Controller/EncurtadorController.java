ddb8780649121b01a2de830fe509679bbd97ceec
victornext <victorbrs517@gmail.com>
2025-07-09 23:00:17 -0300

    Final

--- src/main/java/com/victornext/encurtador/Controller/EncurtadorController.java
+++ src/main/java/com/victornext/encurtador/Controller/EncurtadorController.java
@@ -1,7 +1,35 @@
 package com.victornext.encurtador.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.victornext.encurtador.DTOs.UrlRequest;
+import com.victornext.encurtador.model.Encurtador;
+import com.victornext.encurtador.service.EncurtadorService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
+@Data
+@AllArgsConstructor
 public class EncurtadorController {
+
+    private final EncurtadorService service;
+
+
+    @PostMapping("/encurtar")
+    public ResponseEntity<String> encurtarURL(@RequestBody UrlRequest request) {
+        String shortCode = service.encurtarURL(request.getUrl());
+        return ResponseEntity.ok("http://localhost:8080/" + shortCode);
+    }
+
+
+    @GetMapping("/{shortcode}")
+    public ResponseEntity<String> buscarUrlOriginal(@PathVariable String shortcode) {
+        return service.buscarOriginalUrl(shortcode)
+                .map(url -> ResponseEntity.ok("URL: " + url))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
---
+++ src/main/java/com/victornext/encurtador/DTOs/UrlRequest.java
@@ -0,0 +1,10 @@
+package com.victornext.encurtador.DTOs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UrlRequest {
+    private String url;
+}
