0363d9742aa567850401060d3aff30961664cdd6
victornext <victorbrs517@gmail.com>
2025-07-09 22:14:00 -0300

    Correção do repository

--- src/main/java/com/victornext/encurtador/repository/EncurtadorRepository.java
+++ src/main/java/com/victornext/encurtador/repository/EncurtadorRepository.java
@@ -4,7 +4,10 @@ import com.victornext.encurtador.model.Encurtador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface EncurtadorRepository extends JpaRepository<Long, Encurtador> {
+public interface EncurtadorRepository extends JpaRepository<Encurtador, Long> {
+    Optional<Encurtador> findByShortCode(String shortCode);
 
 }
